82d95dcbc9e80c0e0998644074ce2005f9ac8864
Jeegar Patel <jeegar.patel@xilinx.com>
2019-07-25 23:20:38 -0700

    Revert "Update name of ctrlsw level test application binaries"
    This reverts commit e12b4bcfbbc5b8bea759873106852ad6a32f5ffc.

--- exe_encoder/project.mk
+++ exe_encoder/project.mk
@@ -49,14 +49,14 @@ $(BIN)/$(THIS_EXE_ENCODER)/main.cpp.o: INTROSPECT_FLAGS=-DAL_COMPIL_FLAGS='"$(CF
 $(BIN)/$(THIS_EXE_ENCODER)/main.cpp.o: INTROSPECT_FLAGS+=-DHAS_COMPIL_FLAGS=1
 
 
-$(BIN)/ctrlsw_encoder: $(EXE_ENCODER_OBJ) $(LIB_REFENC_A) $(LIB_ENCODER_A)
+$(BIN)/AL_Encoder.exe: $(EXE_ENCODER_OBJ) $(LIB_REFENC_A) $(LIB_ENCODER_A)
 
-TARGETS+=$(BIN)/ctrlsw_encoder
+TARGETS+=$(BIN)/AL_Encoder.exe
 
 ifndef ($(ENABLE_LIB_ISCHEDULER),0)
-$(BIN)/AL_Encoder.sh: $(BIN)/ctrlsw_encoder
+$(BIN)/AL_Encoder.sh: $(BIN)/AL_Encoder.exe
 	@echo "Generate script $@"
-	$(shell echo 'LD_LIBRARY_PATH=$(BIN) $(BIN)/ctrlsw_encoder "$$@"' > $@ && chmod a+x $@)
+	$(shell echo 'LD_LIBRARY_PATH=$(BIN) $(BIN)/AL_Encoder.exe "$$@"' > $@ && chmod a+x $@)
 
 TARGETS+=$(BIN)/AL_Encoder.sh
 endif
